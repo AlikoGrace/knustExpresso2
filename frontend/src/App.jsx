45868b1ab6caf96c94918eb1f08b30ee8f84f1ee
Grace Aliko <aliko@kali.gracealiko.com>
2023-11-16 09:55:09 +0000

    worked on the signup ui

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Home from './pages/Home'
 import { Outlet } from 'react-router-dom'
+import SignIn from './pages/SignIn'
 
 const App = () => {
   return (
     <div> 
-    <Outlet/>
+    {/* <Outlet/> */}
+    <SignIn/>
 
     </div>
     
